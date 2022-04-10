9eb692d558cad0da020058c4d1e586c39eb91179
micha24goll <103368516+micha24goll@users.noreply.github.com>
2022-04-10 16:45:46 +0300

    Update Ping_Pong_main.py

--- Ping_Pong_main.py
+++ Ping_Pong_main.py
@@ -13,3 +13,19 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
         
   #Create class 'GameSprite'.
+class Player(GameSparite):
+    def update_r(self):
+        keys = key.got_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height = 80:
+            self.rect.y += self.speed
+        
+   def update_r(self):
+        keys = key.got_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height = 80:
+            self.rect.y += self.speed
+            
+     #Create class 'Player'
