8dd0f6dba1347b958f10e81155aeb4e383dc64b3
micha24goll <103368516+micha24goll@users.noreply.github.com>
2022-04-10 16:39:39 +0300

    Update Ping_Pong_main.py

--- Ping_Pong_main.py
+++ Ping_Pong_main.py
@@ -1,1 +1,15 @@
-#Первый комментарий
+from pygame import *
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, weight, height):
+
+        self.image = transform.scale(image.load(player_image), (weight, height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+        
+  #Create class 'GameSprite'.
